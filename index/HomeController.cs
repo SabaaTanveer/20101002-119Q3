8f3bb0c580161dc5d12039a4b8946f495b1a59a3
SabaaTanveer <149601503+SabaaTanveer@users.noreply.github.com>
2024-01-17 12:53:26 -0800

    Add files via upload

---
+++ index/HomeController.cs
@@ -0,0 +1,90 @@
+﻿using my_wayshop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace my_wayshop.Controllers
+{
+
+    public class HomeController : Controller
+    {
+
+        public ActionResult Indexuser()
+        {
+            return View();
+        }
+
+
+        public ActionResult Indexadmin()
+        {
+            return View();
+        }
+
+        Model1 db = new Model1();
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(M_Admin a)
+        {
+            int r = db.M_Admin.Where(x => x.Admin_Email == a.Admin_Email && x.Admin_Password == a.Admin_Password).Count();
+
+            if (r == 1)
+            {
+                return RedirectToAction("Indexadmin", "Home");
+            }
+            else
+            {
+                ViewBag.Message = "Invalid credentials";
+                return View();
+            }
+
+
+        }
+
+
+        public ActionResult About()
+        {
+
+            return View();
+        }
+
+
+        public ActionResult Cart()
+        {
+
+            return View();
+        }
+
+       
+
+        public ActionResult Feedback()
+        {
+
+            return View();
+        }
+
+        public ActionResult shop(int? id)
+        {
+            shopmodel s = new shopmodel();
+            s.cat = db.M_Category.ToList();
+            if (id == null)
+                s.prod = db.M_Product.ToList();
+            else
+                s.prod = db.M_Product.Where(p => p.Product_Category_FID == id).ToList();
+            return View(s);
+        }
+        public ActionResult Contact()
+        {
+
+
+            return View();
+        }
+
+
+    }
+}
